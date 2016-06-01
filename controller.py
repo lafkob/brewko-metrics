1850849707392878a6aed6abca21cbd9f8e5473a
lafkob <lafkob@gmail.com>
2016-05-31 20:00:45 -0400

    pitchers in table, sorted by game score

--- controller.py
+++ controller.py
@@ -11,8 +11,10 @@ def app_root():
 
     :return:
     """
+    # TODO: pass in sort column, sort direction, and date
     now = datetime.datetime.now()
     players = stats.get_daily_starting_pitcher_stats(now.year, now.month, now.day)
+    players.sort(key=lambda p: p.game_score, reverse=True)
     return render_template("index.html", players=players)
 
 
