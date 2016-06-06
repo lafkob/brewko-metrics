bc54527e703e76ef0e6d31fc2cb6295049305aa6
John Brewer <brewerja@gmail.com>
2016-06-05 22:37:39 -0400

    Change park factor calculation
    Now use the avg runs in the rgular season at a park in the same season
    as the game in question.

--- etl/park_factor.py
+++ etl/park_factor.py
@@ -7,46 +7,40 @@ class ParkFactor(object):
     def __init__(self, db_file):
         self.conn = sqlite3.connect(db_file)
 
-    def get_avg_runs_at_site_in_last_100_games(self, site, date):
-        query = ("SELECT sum(total_runs) FROM "
-                 "(SELECT total_runs FROM games "
-                 " WHERE site = '{}' AND date < '{}' "
-                 " ORDER BY date DESC limit 100)").format(site, date)
-        cursor = self.conn.cursor()
-        cursor.execute(query)
-        return cursor.fetchone()[0] / 100
-
-    def games_before_date(self, site, date):
-        query = ("SELECT count(*) FROM games "
-                 "WHERE site = '{}' AND date < '{}'").format(site, date)
+    def get_site_reg_season_stats(self, site, year):
+        query = ("SELECT count(*), sum(total_runs) FROM games "
+                 "WHERE site = '{}' AND postseason=0 AND "
+                 "strftime('%Y', date) =  '{}'").format(site, year)
         cursor = self.conn.cursor()
         cursor.execute(query)
         result = cursor.fetchone()
         if result:
-            return int(result[0])
+            return int(result[0]), int(result[1])
         return 0
 
-    def get_park_factor(self, site, date):
-        if self.games_before_date(site, date) > 99:
-            avg_runs = self.get_avg_runs_at_site_in_last_100_games(site, date)
+    def get_park_factor(self, site, year):
+        num_games, total_runs = self.get_site_reg_season_stats(site, year)
+        if num_games >= 50:
+            avg_runs = total_runs / num_games
             return 2 * avg_runs - 18
         return 0
 
     def set_park_factors(self):
         outing_cursor = self.conn.cursor()
         update_cursor = self.conn.cursor()
-        outing_cursor.execute('SELECT id, site, date FROM starts')
+        outing_cursor.execute("SELECT id, site, strftime('%Y', date) as year "
+                              "FROM starts")
         i = 1
         while True:
             outing = outing_cursor.fetchone()
             if not outing:
                 break
             i += 1
-            if i % 1000 == 0:
+            if i % 100 == 0:
                 self.conn.commit()
                 print(i)
-            id, site, date = outing
-            factor = pf.get_park_factor(site, date)
+            id, site, year = outing
+            factor = pf.get_park_factor(site, year)
             update_cursor.execute('UPDATE starts SET park_factor = {} '
                                   'where id = {}'.format(factor, id))
         self.conn.commit()
